1f07d7180123a2d4af5dc520f154cca1b1898a67
bert2 <shuairan@gmail.com>
2023-01-07 14:33:30 +0100

    implement char class with range

--- src/MyGrep/Parser.hs
+++ src/MyGrep/Parser.hs
@@ -1,7 +1,9 @@
 module MyGrep.Parser (parseRegex) where
 
+import Control.Monad
 import Data.Bifunctor
 import Data.Functor ((<&>), ($>))
+import Data.List
 import Data.Maybe
 import Data.Void (Void)
 import MyGrep.NFA.Build qualified as NFA
@@ -22,13 +24,13 @@ regex = do
 
 regex' :: Parser NFA.StateB
 regex' = choice [
-  wordCharClass    $> NFA.oneOf [NFA.charRange ('0', '9'),
-                                 NFA.charRange ('A', 'Z'),
-                                 NFA.charRange ('a', 'z'),
-                                 NFA.literalChar '_'],
-  digitCharClass   $> NFA.charRange ('0', '9'),
-  escapedChar     <&> NFA.literalChar,
-  literalChar     <&> NFA.literalChar]
+  wordCharClass   $> NFA.oneOf [NFA.charRange ('0', '9'),
+                                NFA.charRange ('A', 'Z'),
+                                NFA.charRange ('a', 'z'),
+                                NFA.literalChar '_'],
+  digitCharClass  $> NFA.charRange ('0', '9'),
+  charClass      <&> NFA.charRange,
+  litOrEscChar   <&> NFA.literalChar]
 
 startAnchorOrAnyString :: Parser NFA.StateB
 startAnchorOrAnyString = maybe NFA.anyString (const mempty) <$> optional (char '^')
@@ -37,13 +39,26 @@ endAnchorOrAnyString :: Parser NFA.StateB
 endAnchorOrAnyString = maybe NFA.anyString (const mempty) <$> optional (char '$')
 
 digitCharClass :: Parser ()
-digitCharClass = () <$ string "\\d"
+digitCharClass = () <$ string "\\d" <?> "digit character class"
 
 wordCharClass :: Parser ()
-wordCharClass = () <$ string "\\w"
+wordCharClass = () <$ string "\\w" <?> "word character class"
 
-literalChar :: Parser Char
-literalChar = noneOf ['^', '$', '\\'] <?> "literal character"
+charClass :: Parser (Char, Char)
+charClass = between (char '[') (char ']') charRange
+  where charRange = (,) <$> litOrEscChar <* char '-' <*> litOrEscChar
+        litOrEscChar = charWithReserved "^-\\[]"
 
-escapedChar :: Parser Char
-escapedChar = char '\\' *> oneOf "^$\\" <?> "'^', '$', or '\'"
+litOrEscChar :: Parser Char
+litOrEscChar = charWithReserved "^$\\[]"
+
+charWithReserved :: [Char] -> Parser Char
+charWithReserved res = escChar <|> litChar
+  where litChar = noneOf res <?> "any character (except " ++ resLbl ++ ")"
+        escChar = char '\\' *> resChar <?> "escape sequence"
+        resChar = oneOf res <?> resLbl
+        resLbl = pprintChars res
+
+pprintChars :: [Char] -> String
+pprintChars chars = (mconcat . intersperse ", " . init) quoted ++ ", or " ++ last quoted
+  where quoted = map (\c -> "'" ++ [c] ++ "'") chars
